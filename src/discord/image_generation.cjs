61a7385d1a202a8909b99f27704439208c4b3ede
Lambda-fr <atserio@live.fr>
2025-07-21 18:07:51 -0400

    Move  word wrapping functionality into image_generation.cjs

--- src/discord/image_generation.cjs
+++ src/discord/image_generation.cjs
@@ -3,7 +3,6 @@ const { AttachmentBuilder, EmbedBuilder, ActionRowBuilder } = require('discord.j
 const { ASSETS_PATH } = require('../utils/paths').default;
 const path = require('path');
 const { backButton, forwardButton } = require(path.join(ASSETS_PATH, 'buttons.js'));
-const { printAtWordWrap } = require(path.join(ASSETS_PATH, 'utils.js'))
 const Canvas = require('canvas');
 require('chartjs-adapter-moment')
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
@@ -36,6 +35,44 @@ function calculateWordWrapLines(context, text, maxWidth) {
     }
     return lineCount;
 }
+
+function printAtWordWrap(context, text, x, y, lineHeight, fitWidth) {
+    fitWidth = fitWidth || 0;
+    lineHeight = lineHeight || 20;
+
+    var currentLine = 0;
+
+    var lines = text.split(/\r\n|\r|\n/);
+    for (var line = 0; line < lines.length; line++) {
+
+
+        if (fitWidth <= 0) {
+            context.fillText(lines[line], x, y + (lineHeight * currentLine));
+        } else {
+            var words = lines[line].split(' ');
+            var idx = 1;
+            while (words.length > 0 && idx <= words.length) {
+                var str = words.slice(0, idx).join(' ');
+                var w = context.measureText(str).width;
+                if (w > fitWidth) {
+                    if (idx == 1) {
+                        idx = 2;
+                    }
+                    context.fillText(words.slice(0, idx - 1).join(' '), x, y + (lineHeight * currentLine));
+                    currentLine++;
+                    words = words.splice(idx - 1);
+                    idx = 1;
+                }
+                else { idx++; }
+            }
+            if (idx > 0)
+                context.fillText(words.join(' '), x, y + (lineHeight * currentLine));
+        }
+        currentLine++;
+    }
+    return currentLine;
+}
+
 
 async function displayNewAchievementImage(achievement, game, users, guild, unlockingUser, position) {
     try {
